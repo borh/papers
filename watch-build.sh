f7ada6560d748c8daf80a3c8fe790b08ebbaae77
Bor Hodošček <bor.hodoscek@gmail.com>
2013-10-16 11:11:35 +0900

    add table fix script

--- watch-build.sh
+++ watch-build.sh
@@ -7,7 +7,7 @@ while true; do
     pandoc --no-wrap --highlight-style=kate --template=article-template.pandoc --biblatex --latex-engine=lualatex \
            -f markdown \
            --smart \
-           --bibliography=bibliography/doctors-bibliography.bib -t latex $1 -o tmp-$bn.tex && \
+           --bibliography=bibliography/doctors-bibliography.bib -t latex $1 | ./fix-tables.py > tmp-$bn.tex && \
            lualatex --shell-escape --interaction=batchmode tmp-$bn ; biber -U -u tmp-$bn ; mv tmp-$bn.pdf $bn.pdf && cp $bn.pdf ~/Dropbox/My\ Papers/
 done
 # | ./fix-tables.py
