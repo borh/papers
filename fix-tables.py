83c0c00ad104b1eb31fc3259176d9651adc92415
Bor Hodošček <bor.hodoscek@gmail.com>
2013-11-12 18:13:17 +0900

    use Figure for n-column layout

--- fix-tables.py
+++ fix-tables.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
 
     for line in sys.stdin:
         line = line.rstrip()
+        line = re.sub('\\\\begin{figure}.*', '\\\\begin{Figure}', line)
+        line = re.sub('\\\\end{figure}', '\\\\end{Figure}', line)
         if re.search('\\\\begin{.*longtable.*', line):
             in_table = True
             t_done = False
